44ee4e4c463a617ae59e6d20052f5836f5ef457c
Omayowfade <omomayowa.f.adebanjo@vanderbilt.edu>
2018-09-28 23:01:43 -0500

    Wrote Design Document; built RandomCard class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,21 +3,17 @@ import java.util.*;
 
 
 public class Main {
-    private static final List<String> COLORS  =
-            Arrays.asList("RED", "BLACK");
-    private static final List<String> RANKS  =
-            Arrays.asList("ACE" ,"KING", "QUEEN", "JACK",
-                    "TEN", "NINE", "EIGHT", "SEVEN", "SIX",
-                    "FIVE", "FOUR", "THREE", "TWO");
-    private static final List<String> SUITS  =
-            Arrays.asList("SPADES", "CLUBS", "DIAMONDS", "HEARTS");
+    //STATIC CARD ATTRIBUTE DATA
+    private static final List<String> COLORS  = Arrays.asList("RED", "BLACK");
+    private static final List<String> RANKS  = Arrays.asList("ACE" ,"KING", "QUEEN", "JACK", "TEN", "NINE", "EIGHT", "SEVEN", "SIX", "FIVE", "FOUR", "THREE", "TWO");
+    private static final List<String> SUITS  = Arrays.asList("SPADES", "CLUBS", "DIAMONDS", "HEARTS");
+
+    // SESSION SCORE
     private static int SCORE = 0;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         // CARD GAME INTRODUCTION
-        String cont;
         do {
             SCORE = 0;
             header();
@@ -36,9 +32,9 @@ public class Main {
     // DETERMINE RESULTS
     public static boolean printRes(boolean res){
         if(res) {
-            System.out.println("Correct!");
+            System.out.println("CORRECT!");
         } else {
-            System.out.println("Incorrect! Back to Step One!");
+            System.out.println("INCORRECT! Back to Step One!");
         }
         return res;
     }
@@ -48,27 +44,27 @@ public class Main {
         System.out.println("\n**STEP ONE**");
         System.out.println("Nous allons! Guess the next card's color (RED or BLACK): ");
         String input = sc.nextLine().toUpperCase();
-        List<String> randCard = generateRandomCard();
+        RandomCard randCard = new RandomCard();
         System.out.println("The color you picked was: " + input);
-        System.out.println("The next card generated was: " + printRandomCard(randCard));
-        if(printRes(randCard.get(0).equals(input))) stepTwo(sc, randCard);
+        System.out.println("The next card generated was: " + randCard.printCard());
+        if(printRes(randCard.getColor().equals(input))) stepTwo(sc, randCard);
         else{
             SCORE += 3;
             stepOne(sc);
         }    }
 
     // STEP 2 - RANK GUESS
-    public static void stepTwo(Scanner sc, List<String> randPrev){
+    public static void stepTwo(Scanner sc, RandomCard randPrev){
         System.out.println("\n**STEP TWO**");
         System.out.println("Almost there! Is the next card's rank higher than the last? (Y or N):");
         String input = sc.nextLine().toUpperCase();
-        List<String> randCard = generateRandomCard();
+        RandomCard randCard = new RandomCard();
         System.out.println("The rank you picked was: " + input);
-        System.out.println("The previous card generated was: " + printRandomCard(randPrev));
-        System.out.println("The next card generated was: " + printRandomCard(randCard));
+        System.out.println("The previous card generated was: " + randCard.printCard());
+        System.out.println("The next card generated was: " + randCard.printCard());
         boolean res = (input.equals("Y")) ?
-                RANKS.indexOf(randCard.get(1)) <= RANKS.indexOf(randPrev.get(1)):
-                RANKS.indexOf(randCard.get(1)) >= RANKS.indexOf(randPrev.get(1));
+                RANKS.indexOf(randCard.getRank()) <= RANKS.indexOf(randPrev.getRank()):
+                RANKS.indexOf(randCard.getRank()) >= RANKS.indexOf(randPrev.getRank());
         if(res) stepThree(sc);
         else{
             SCORE += 2;
@@ -81,10 +77,10 @@ public class Main {
         System.out.println("\n**STEP THREE**");
         System.out.println("Last one! Guess the next card's suit (SPADES, HEARTS, DIAMONDS, or CLUBS):");
         String input = sc.nextLine().toUpperCase();
-        List<String> randCard = generateRandomCard();
+        RandomCard randCard = new RandomCard();
         System.out.println("The suit you picked was: " + input);
-        System.out.println("The suit card generated was: " + printRandomCard(randCard));
-        if(printRes(randCard.get(2).equals(input))){
+        System.out.println("The suit card generated was: " + randCard.getSuit());
+        if(printRes(randCard.getSuit().equals(input))){
             System.out.println("\nCongratulations! You've beaten Deviation!");
             System.out.println("Your score was: " + SCORE);
         } else{
@@ -93,32 +89,53 @@ public class Main {
         }
     }
 
-    // RANDOM CARD GENERATOR (ARRAY - {COLOR, RANK, SUIT])
-    public static List<String> generateRandomCard(){
-        List<String> card = new LinkedList<String>();
-        card.add(generateRandomColor());
-        card.add(generateRandomRank());
-        card.add(generateRandomSuit());
-        return card;
+    public static class RandomCard{
+
+        // RANDOM CARD ATTRIBUTES
+        private String color;
+        private String rank;
+        private String suit;
+
+        // ASSIGN RANDOM ATTRIBUTES TO GENERATED CARD
+        public RandomCard(){
+            color = generateRandomColor();
+            rank = generateRandomRank();
+            suit = generateRandomSuit();
+        }
+
+        // ACCESSOR FUNCTIONS
+        public String getColor(){
+            return color;
+        }
+
+        public String getRank(){
+            return rank;
+        }
+
+        public String getSuit(){
+            return suit;
+        }
+
+        // RANDOM COLOR GENERATOR
+        public String generateRandomColor(){
+            return COLORS.get((int) Math.floor((Math.random() * COLORS.size())));
+        }
+
+        // RANDOM RANK GENERATOR
+        public String generateRandomRank(){
+            return RANKS.get((int) Math.floor((Math.random() * RANKS.size())));
+        }
+
+        // RANDOM SUIT GENERATOR
+        public String generateRandomSuit(){
+            return SUITS.get((int)Math.floor((Math.random() * SUITS.size())));
+        }
+
+        // PRINT OUT CARD
+        public String printCard(){
+            return (getColor() + " " + getRank() + " of " + getSuit());
+        }
     }
 
-    // RANDOM CARD PRINTER (STRING)
-    public static String printRandomCard(List<String> card){
-        return (card.get(0) + " " + card.get(1) + " of " + card.get(2));
-    }
-
-    // RANDOM COLOR GENERATOR
-    public static String generateRandomColor(){
-        return COLORS.get((int) Math.floor((Math.random() * COLORS.size())));
-    }
-
-    // RANDOM RANK GENERATOR
-    public static String generateRandomRank(){
-        return RANKS.get((int) Math.floor((Math.random() * RANKS.size())));
-    }
-
-    // RANDOM SUIT GENERATOR
-    public static  String generateRandomSuit(){
-        return SUITS.get((int)Math.floor((Math.random() * SUITS.size())));
-    }
 }
+
